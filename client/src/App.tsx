67e22c81b3c0833d4a39c2318f3174b391a8dec2
PruthvidharReddy01 <pruthvidharreddykotha@gmail.com>
2024-12-15 17:25:41 +0530

    Final version

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,7 @@ import VideoConnectionDialog from './components/VideoConnectionDialog'
 import Chat from './components/Chat'
 import HelperButtonGroup from './components/HelperButtonGroup'
 import MobileVirtualJoystick from './components/MobileVirtualJoystick'
+import Sidebar from './components/Sidebar' // Import the Sidebar component
 
 const Backdrop = styled.div`
   position: absolute;
@@ -28,35 +29,29 @@ function App() {
   let ui: JSX.Element
   if (loggedIn) {
     if (computerDialogOpen) {
-      /* Render ComputerDialog if user is using a computer. */
       ui = <ComputerDialog />
     } else if (whiteboardDialogOpen) {
-      /* Render WhiteboardDialog if user is using a whiteboard. */
       ui = <WhiteboardDialog />
     } else {
       ui = (
-        /* Render Chat or VideoConnectionDialog if no dialogs are opened. */
         <>
           <Chat />
-          {/* Render VideoConnectionDialog if user is not connected to a webcam. */}
           {!videoConnected && <VideoConnectionDialog />}
           <MobileVirtualJoystick />
         </>
       )
     }
   } else if (roomJoined) {
-    /* Render LoginDialog if not logged in but selected a room. */
     ui = <LoginDialog />
   } else {
-    /* Render RoomSelectionDialog if yet selected a room. */
     ui = <RoomSelectionDialog />
   }
 
   return (
     <Backdrop>
       {ui}
-      {/* Render HelperButtonGroup if no dialogs are opened. */}
       {!computerDialogOpen && !whiteboardDialogOpen && <HelperButtonGroup />}
+      {roomJoined && <Sidebar />}
     </Backdrop>
   )
 }
